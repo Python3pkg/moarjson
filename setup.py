71e2a6e70f8c76cf1339179b871d60a7aa3174c3
Niels Lemmens <draso.odin@gmail.com>
2014-01-31 13:28:38 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
 
+def read_file(filename):
+    try:
+        f = open(filename)
+        return f.read()
+    finally:
+        f.close()
+
+
 setup(
     name='moarjson',
-    version='0.2',
+    version='0.2.1',
     url='https://github.com/Bulv1ne/Moarjson',
     license='MIT',
     author='Niels Lemmens',
     author_email='draso.odin@gmail.com',
-    description='',
+    description='Easily json dump types and classes',
+    long_description=read_file('README.md'),
     packages=['moarjson'],
     platforms='any',
     install_requires=[],
