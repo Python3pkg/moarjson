cee1f54f90701bf706f18a372ec80aa17cd467b7
Niels Lemmens <draso.odin@gmail.com>
2014-02-08 17:39:01 +0100

    Add test_iter.py

---
+++ tests/test_iter.py
@@ -0,0 +1,11 @@
+import collections
+
+import moarjson as json
+
+
+json.register(collections.Iterable, list)
+
+def test_iter():
+    a_list = ['a', 'b', 'c']
+    an_iterable = iter(a_list)
+    assert json.dumps(an_iterable) == json.dumps(a_list)
